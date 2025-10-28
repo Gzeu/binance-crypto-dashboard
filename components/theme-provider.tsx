bcd43bc31f035ab6da0d585f3affb1529c42e4b4
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-10-28 09:54:34 +0200

    Update ThemeProvider to prevent hydration mismatch with suppressHydrationWarning

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,21 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      storageKey="binance-dashboard-theme"
+      disableTransitionOnChange={false}
+      {...props}
+    >
+      <div suppressHydrationWarning>
+        {children}
+      </div>
+    </NextThemesProvider>
+  )
 }
